590fee99a7cd0f03d381501ca2503601ffe4d241
aris-ramos <82551184+aris-ramos@users.noreply.github.com>
2021-05-23 15:15:54 -0500

    Add files via upload

---
+++ P3_3ParImpar.c
@@ -0,0 +1,18 @@
+/*Dep3_Prog2:
+Verificar si es par o impar*/
+#include <stdio.h>
+
+int main()
+{
+    printf("\n Introduce un numero");  
+    scanf("%d", &numero);
+
+    if ( numero%2 ==0)
+    {
+        printf("\n El numero es PAR");
+    }
+    else
+    {
+        printf("\n El numero es IMPAR");
+    }
+}
